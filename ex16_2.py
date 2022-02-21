68b8fe7626555998a6ebc3aa0148100f91f07582
RuSatin <>
2022-02-21 10:21:46 +0000

    Initial commit

---
+++ ex16_2.py
@@ -0,0 +1,62 @@
+# languages = ['Python', 'Java', 'JS', 'SQL']
+# languages.insert(2, input('Введите язык программирования: '))
+# ind_lang = languages.index(input('Индекс какого ЯП вытащить?'))
+# print(languages)
+# print(ind_lang)
+
+# films = [
+#     'Крепкий орешек', 'Назад в будущее', 'Таксист', 
+#     'Леон', 'Богемская рапсодия', 'Город грехов',
+#     'Мементо', 'Отступники', 'Деревня', 
+#     'Проклятый остров', 'Начало', 'Матрица'
+# ]
+
+# my_list=[]
+# while True:
+#   print("Ващ топ фильмов:", my_list)
+#   new_movie = input('')
+
+# a = [1, 5, 3]
+# b = [1, 5, 1, 5]
+# c = [1, 3, 1, 5, 3, 3]
+# for i in b:
+#     a.append(i)
+# t = 0
+# for i in a:
+#     if i == 5:
+#         t += 1
+# print(t)
+# d = []
+# for i in a:
+#     if i != 5:
+#         d.append(i)
+# for i in c:
+#     d.append(i)
+# t = 0
+# for i in d:
+#     if i == 3:
+#         t += 1
+# print(t)
+# print(d)
+
+# zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
+# zoo.insert(1, 'bear')
+# print(zoo)
+# zoo.remove('elephant')
+# print(zoo)
+# print("Лев сидит в клетке", zoo.index('lion')+1)
+# print("Обезьяна сидит в клетке", zoo.index('monkey')+1)
+
+n = []
+k = int(input(('Кол-во сотрудников: ')))
+for i in range(k):
+  j = int(input('ЗП: '))
+  n.append(j)
+print(n)
+for o in n:
+  if o == 0:
+    n.remove(o)
+print("Осталось сотрудников:", len(n))
+print(n)
+print("Максимальная ЗП:", max(n))
+print("Минимальная ЗП:", min(n))
---
+++ main.py
@@ -0,0 +1,62 @@
+import ex16_2
+# import this
+
+# scores = [1, 2, 3, 4, 5]
+# print(scores)
+# scores[3] *= 3
+# for i in scores:
+#   if scores[-1] >= 5:
+#     scores[-1] = 0
+#   if scores[0] < 1:
+#     scores[0] += 10
+# print(scores)
+
+# print(scores)
+# scores.append('hello')
+# print(scores)
+
+# monsters_count = int(input("Введите кол-во монстров: "))
+# mage_index = int(input("Номер мага в списке: "))
+# monsters_damage = []
+
+# for monster in range(1, monsters_count+1):
+#   print("Урон у", monster, 'монстра', end=': ')
+#   damage = int(input(''))
+#   monsters_damage.append(damage)
+# print(monsters_damage)
+
+# nums_list = []
+# n = int(input('Кол-во чисел в списке: '))
+
+# for _ in range(1, n+1):
+#  num = int(input('Очередное число: '))
+#  nums_list.append(num)
+# print(nums_list)
+
+# maximum = 1
+# minimum = -1
+
+# for i in nums_list:
+#  if maximum < nums_list[i]:
+#    maximum = i
+#  if minimum > nums_list[i]:
+#    minimum = i
+
+# print('Максимальное число в списке:', maximum)
+# print('Минимальное число в списке:', minimum)
+
+# name = 'Руслан'
+# slicer_name = list(name)
+# print(slicer_name)
+# for j in slicer_name:
+#   if j == 'а':
+#     slicer_name.remove(j)
+
+# words_list = []
+# counts = [0, 0, 0]
+# for i in raange(3):
+#   print("Введите", i+1, "слово: ", end=' ')
+#   word = input()
+#   word_list.append(word)
+
+# text = input("Слово из текста")
